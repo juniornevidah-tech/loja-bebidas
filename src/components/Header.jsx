599c3ca3006574f43769caef58fbca5822fabef6
Sidney junior <juniornevidah@gmail.com>
2026-08-12 14:32:00 -0300

    muda

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header({ abrirSidebar, buscar, aoBuscar, abrirCarrinho }) {
     return(
         <header className="flex justify-between items-center sticky top-0 z-50 bg-white shadow-md px-6 py-4">
             <h1 className="text-2xl font-bold text-vinho">{dados.loja.nome}</h1>
-            <input type="text" value={buscar} onChange={(e) => aoBuscar(e.target.value)} className="border rounded-full px-4 py-2"/>
+            <input type="text" value={buscar} onChange={(e) => aoBuscar(e.target.value)} className="border rounded-full px-4 py-2" placeholder="Procura..."/>
             <span onClick={abrirCarrinho}>Carrinho({somaTotal})</span>
             <button onClick={abrirSidebar} className="text-2xl hover:scale-110 transition">≡</button>
         </header>
